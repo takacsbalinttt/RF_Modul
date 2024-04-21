0d3e7845ebfc9081f05312b1f9801ceb1055f7d4
mslevi <ms.levi2003@gmail.com>
2024-04-21 20:58:09 +0200

    started hotcakes connection implementation

--- DNN.YounglingSlayer.WokPicker/Components/HotcakesComponent.cs
+++ DNN.YounglingSlayer.WokPicker/Components/HotcakesComponent.cs
@@ -4,8 +4,37 @@ using DotNetNuke.Data;
 using DotNetNuke.Framework;
 using System.Collections.Generic;
 
-/*
+
 namespace DNN.WokPickerDNN.YounglingSlayer.WokPicker.Components
 {
-    internal interface 
-}*/
+    internal interface IHotCakesManager
+    {
+        IEnumerable<Item> ReadHotCakes(string HCID);
+        
+    }
+
+    internal class HotCakesManager : ServiceLocator<IHotCakesManager, HotCakesManager>, IHotCakesManager
+    {
+        public IEnumerable<Item> ReadHotCakes(string HCID)
+        {
+            /// READ HOTCAKES PRODUCTS!!!!
+            
+            IEnumerable<Item> product;
+            using (IDataContext ctx = DataContext.Instance())
+            {
+                var rep = ctx.GetRepository<Item>();
+                product = rep.Get(HCID);
+            }
+            
+            
+            return null;
+        }
+
+        protected override System.Func<IHotCakesManager> GetFactory()
+        {
+            return () => new HotCakesManager();
+        }
+
+    }
+
+}
