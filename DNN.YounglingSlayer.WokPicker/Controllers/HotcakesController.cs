79a5b5c09a60f85a656076b0811e7b836dd5581f
mslevi <ms.levi2003@gmail.com>
2024-04-22 20:49:15 +0200

    made filtering the hotcakes list possible via a variable

--- DNN.YounglingSlayer.WokPicker/Controllers/HotcakesController.cs
+++ DNN.YounglingSlayer.WokPicker/Controllers/HotcakesController.cs
@@ -17,10 +17,16 @@ namespace DNN.WokPickerDNN.YounglingSlayer.WokPicker.Controllers
     [DnnHandleError]
     public class HotcakesController : DnnController
     {
+        public string selected_bvin = "asd";
         [ModuleAction]
         public ActionResult Index()
         {
             var items = HotCakesManager.Instance.ReadHotCakes();
+            return View(items);
+        }
+        public ActionResult FindBVIN(string bvin)
+        {
+            var items = HotCakesManager.Instance.GetBybvin(bvin);
             return View(items);
         }
 
