1b37b11df077f70e84cddc6ce4f0dde892dd547c
mslevi <ms.levi2003@gmail.com>
2024-04-23 19:17:27 +0200

    Prepare connection to translation table

---
+++ DNN.YounglingSlayer.WokPicker/Models/ProductTranslations.cs
@@ -0,0 +1,30 @@
+﻿
+using DotNetNuke.ComponentModel.DataAnnotations;
+using System;
+using System.Security.Cryptography.X509Certificates;
+using System.Web.Caching;
+
+namespace DNN.WokPickerDNN.YounglingSlayer.WokPicker.Models
+{
+    [TableName("hcc_ProductTranslations")]
+    //setup the primary key for table
+    [PrimaryKey("ProductTranslationId")]
+    //configure caching using PetaPoco
+    [Cacheable("ProductTranslations", CacheItemPriority.Default, 20)]
+    //scope the objects to the ModuleId of a module on a page (or copy of a module on a page)
+    [Scope("ModuleId")]
+    public class ProductTranslations
+    {
+        public int ProductTranslationId { get; set; }
+        
+        /// <summary>
+        /// BVIN
+        /// </summary>
+        public string ProductID { get; set; }
+
+        public string Culture { get; set; }
+        public string ProductName { get; set; }
+
+        public string MetaTitle { get; set; }
+    }
+}
