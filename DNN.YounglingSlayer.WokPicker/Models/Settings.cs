71122d0ab5d29693e24bf0d89948bed7792b1ff4
mslevi <ms.levi2003@gmail.com>
2024-04-23 10:06:55 +0200

    added code for handling selected bvin as a variable from settings

--- DNN.YounglingSlayer.WokPicker/Models/Settings.cs
+++ DNN.YounglingSlayer.WokPicker/Models/Settings.cs
@@ -10,15 +10,16 @@
 ' 
 */
 using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
 
 namespace DNN.WokPickerDNN.YounglingSlayer.WokPicker.Models
 {
     public class Settings
     {
-        public bool Setting1 { get; set; }
+        public bool MultiSelect { get; set; }
         public DateTime Setting2 { get; set; }
+
+        public string Bvin1 { get; set; }   
+        
+        public int NumberOfItems { get; set; }   
     }
 }
