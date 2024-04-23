0212aa77693e892636727bfdcd2fa06c36661054
mslevi <ms.levi2003@gmail.com>
2024-04-23 18:51:01 +0200

    Functioning search, can set product id to search through settings page

--- DNN.YounglingSlayer.WokPicker/Controllers/HotcakesController.cs
+++ DNN.YounglingSlayer.WokPicker/Controllers/HotcakesController.cs
@@ -1,11 +1,15 @@
 ﻿using DNN.WokPickerDNN.YounglingSlayer.WokPicker.Components;
 using DNN.WokPickerDNN.YounglingSlayer.WokPicker.Models;
+using DotNetNuke.Collections;
+using DotNetNuke.Common.Utilities;
+using DotNetNuke.Entities.Modules;
 using DotNetNuke.Entities.Users;
 using DotNetNuke.Framework.JavaScriptLibraries;
 using DotNetNuke.Web.Mvc.Framework.ActionFilters;
 using DotNetNuke.Web.Mvc.Framework.Controllers;
 using System;
 using System.Linq;
+using System.Net.Configuration;
 using System.Web.Mvc;
 
 
@@ -17,18 +21,31 @@ namespace DNN.WokPickerDNN.YounglingSlayer.WokPicker.Controllers
     [DnnHandleError]
     public class HotcakesController : DnnController
     {
-        public string selected_bvin = "asd";
+
         [ModuleAction]
         public ActionResult Index()
         {
+            var settings = this.ActiveModule.ModuleSettings;
             var items = HotCakesManager.Instance.ReadHotCakes();
+            var bvin1 = settings.GetValue<string>("DNN.YounglingSlayer.WokPicker_Bvin1").ToLower();
+            /*
+            if(ModuleContext.Settings.ContainsKey("WokPicker_Bvin1"))
+            {
+                bvin = ModuleContext.Settings["Bvin1"].ToString();
+            }
+
+            */
+            ViewBag.items = FindBVIN(bvin1);
             return View(items);
         }
-        public ActionResult FindBVIN(string bvin)
+        HotCakes FindBVIN(string bvin)
         {
             var items = HotCakesManager.Instance.GetBybvin(bvin);
-            return View(items);
+            
+            return items;
         }
+
+
 
     }
 }
