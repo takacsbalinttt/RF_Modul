c9fcec09d5b9914c7abb57fa3fbb6d506fca89dc
mslevi <ms.levi2003@gmail.com>
2024-04-29 18:37:09 +0200

    Added card settings model

---
+++ DNN.YounglingSlayer.WokPicker/Models/CardSettings.cs
@@ -0,0 +1,28 @@
+﻿/*
+' Copyright (c) 2024 YounglingSlayer
+'  All rights reserved.
+' 
+' THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED
+' TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL
+' THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF
+' CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER
+' DEALINGS IN THE SOFTWARE.
+' 
+*/
+using System;
+
+namespace DNN.WokPickerDNN.YounglingSlayer.WokPicker.Models
+{
+    public class CardSettings
+    {
+        public string bvin { get; set; }   
+        
+        public string nameOverride { get; set; }
+        
+        public string imageOverride { get; set; }
+
+        public bool disable { get; set; }
+
+        public bool spicy { get; set; }
+    }
+}
